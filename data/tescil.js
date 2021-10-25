93889f78f3e0c312d59c6d8150a1ec64efe38a87
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-25 05:42:40 +0300

    25.10.2021

---
+++ data/tescil.js
@@ -0,0 +1,37 @@
+var tescil={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ilce": "Kalecik",
+        "mahalle": "Çandır",
+        "adet": 29,
+        "program": "Tescil"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          33.46790656708493,
+          40.26034101761316
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "ilce": "Ayaş",
+        "mahalle": "Tekke",
+        "adet": 120,
+        "program": "Tescil"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          32.37292595514031,
+          39.91929619189081
+        ]
+      }
+    }
+  ]
+};
---
+++ data/yapiKayit.js
@@ -0,0 +1,21 @@
+var yapiKayit={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "ilce": "Çankaya",
+        "mahalle": "Ellinci Yıl",
+        "adet": 72,
+        "program": "Yapı Kayıt"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          32.88144652804899,
+          39.923547982610785
+        ]
+      }
+    }
+  ]
+};
